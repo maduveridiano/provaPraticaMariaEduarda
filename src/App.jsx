1cc90508e5084c8b5a76e01ddaca7987c8768efa
maduveridiano <dudqaa13@gmail.com>
2024-07-04 08:52:44 -0300

    Refactor code for improved performance and readability

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import ListaClubes from './components/ListaClubes'
+
+function App() {
+
+  return (
+    <div className='paginaListagem'>
+      <Header />
+      <ListaClubes />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="headerIdentifica">
+            <img className= 'imagemLogo' src='./senailogo.png'/>
+            <h1>Maria Eduarda Alves Veridiano - 3ºB</h1>
+        </header>
+    );
+}
